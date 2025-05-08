ceec2300931f0db176632c5fbc1aab6d5d0bc8b6
Nurbek Zhomartov <67108808+NurbakZh@users.noreply.github.com>
2025-05-08 10:24:27 +0500

    fixes 1

--- plugins/parser.py
+++ plugins/parser.py
@@ -189,6 +189,7 @@ def update_lots(cardinal, bot, message):
     else:
         saved_data = []
     
+    print(saved_data)
     # Создаем уникальный ключ для каждого лота, чтобы избежать дублирования
     processed_lots = set()
     
