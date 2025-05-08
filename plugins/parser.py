97f1c4fc2fead9631e96937e808d128d03bbbd6c
Nurbek Zhomartov <67108808+NurbakZh@users.noreply.github.com>
2025-05-08 13:26:51 +0500

    fixes

--- plugins/parser.py
+++ plugins/parser.py
@@ -314,10 +314,14 @@ def update_lots(cardinal, bot, message):
                     logger.debug("TRACEBACK", exc_info=True)
                     break
             except Exception as e:
-                if (e.response.status_code === 429):
+                if e.response.status_code == 429:
                     logger.error(f"[LOTS UPDATE] Не удалось обработать лот {lot_key}. Ошибка: 429")
                     logger.debug("TRACEBACK", exc_info=True)
-                break
+                    break
+                else:
+                    logger.error(f"[LOTS UPDATE] Не удалось обработать лот {lot_key}. Ошибка: необычная")
+                    logger.debug("TRACEBACK", exc_info=True)
+                    break
             time.sleep(20)
 
 def schedule_task(cardinal, bot, message):
